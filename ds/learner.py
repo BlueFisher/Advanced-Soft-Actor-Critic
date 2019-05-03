0ce37fe38aaeb9ec06d80ba58020538d35e9bacd
Fisher Chang <blue_fisher@qq.com>
2019-05-03 10:21:01 +0800

    add distributed sac

---
+++ ds/learner.py
@@ -0,0 +1,270 @@
+import time
+import sys
+import os
+import getopt
+import importlib
+import yaml
+from pathlib import Path
+
+import threading
+import asyncio
+
+import websockets
+from flask import Flask, jsonify, request
+import requests
+
+import numpy as np
+import tensorflow as tf
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from mlagents.envs import UnityEnvironment
+
+NOW = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+
+
+class Learner(object):
+    _replay_host = '127.0.0.1'
+    _replay_port = 8888
+    _learner_port = 8889
+    _websocket_port = 8890
+    _build_path = None
+    _build_port = 5006
+
+    def __init__(self, argv):
+        config = {
+            'name': NOW,
+            'sac': 'sac',
+            'save_model_per_step': 10000
+        }
+        agent_config = dict()
+
+        try:
+            opts, args = getopt.getopt(argv, 'c:en:n:', ['config=',
+                                                         'replay_host',
+                                                         'replay_port',
+                                                         'learner_port=',
+                                                         'build_port=',
+                                                         'name=',
+                                                         'sac='])
+        except getopt.GetoptError:
+            raise Exception('ARGS ERROR')
+
+        for opt, arg in opts:
+            if opt in ('-c', '--config'):
+                with open(arg) as f:
+                    config_file = yaml.load(f, Loader=yaml.FullLoader)
+                    for k, v in config_file.items():
+                        if k == 'replay_host':
+                            self._replay_host = v
+                        elif k == 'replay_port':
+                            self._replay_port = v
+                        elif k == 'learner_port':
+                            self._learner_port = v
+                        elif k == 'websocket_port':
+                            self._websocket_port = v
+                        elif k == 'build_path':
+                            self._build_path = v[sys.platform]
+                        elif k == 'sac_config':
+                            for kk, vv in v.items():
+                                agent_config[kk] = vv
+                        else:
+                            config[k] = v
+                break
+
+        for opt, arg in opts:
+            if opt == 'learner_port':
+                config['learner_port'] == int(arg)
+            elif opt == '--build_port':
+                self._build_port = int(arg)
+            elif opt in ('-n', '--name'):
+                config['name'] = arg.replace('{time}', NOW)
+            elif opt == '--sac':
+                config['sac'] = arg
+
+        if not os.path.exists('config'):
+            os.makedirs('config')
+        with open(f'config/{config["name"]}.yaml', 'w') as f:
+            yaml.dump({**config, **agent_config}, f, default_flow_style=False)
+
+        for k, v in config.items():
+            print(f'{k:>25}: {v}')
+        for k, v in agent_config.items():
+            print(f'{k:>25}: {v}')
+        print('=' * 20)
+
+        self._init_env(config['sac'], config['name'], agent_config)
+        self._run()
+
+    def _init_env(self, sac, name, agent_config):
+        self.env = UnityEnvironment(file_name=self._build_path,
+                                    no_graphics=True,
+                                    base_port=self._build_port)
+
+        self.default_brain_name = self.env.brain_names[0]
+
+        brain_params = self.env.brains[self.default_brain_name]
+        state_dim = brain_params.vector_observation_space_size
+        action_dim = brain_params.vector_action_space_size[0]
+
+        SAC = importlib.import_module(sac).SAC
+        self.sac = SAC(state_dim=state_dim,
+                       action_dim=action_dim,
+                       saver_model_path=f'model/{name}',
+                       summary_path=f'log',
+                       summary_name=name,
+                       **agent_config)
+
+    def _start_policy_evaluation(self):
+        eval_step = 0
+        start_time = time.time()
+        brain_info = self.env.reset(train_mode=True)[self.default_brain_name]
+
+        while True:
+            if self.env.global_done:
+                brain_info = self.env.reset(train_mode=True)[self.default_brain_name]
+
+            len_agents = len(brain_info.agents)
+
+            all_done = [False] * len_agents
+            all_cumulative_rewards = np.zeros(len_agents)
+            hitted = 0
+
+            states = brain_info.vector_observations
+
+            while False in all_done:
+                actions = self.sac.choose_action(states)
+                brain_info = self.env.step({
+                    self.default_brain_name: actions
+                })[self.default_brain_name]
+
+                rewards = np.array(brain_info.rewards)
+                local_dones = np.array(brain_info.local_done, dtype=bool)
+
+                for i in range(len_agents):
+                    if not all_done[i]:
+                        all_cumulative_rewards[i] += rewards[i]
+                        if rewards[i] > 0:
+                            hitted += 1
+
+                    all_done[i] = all_done[i] or local_dones[i]
+
+                states = brain_info.vector_observations
+
+            self.sac.write_constant_summaries([
+                {'tag': 'reward/mean', 'simple_value': all_cumulative_rewards.mean()},
+                {'tag': 'reward/max', 'simple_value': all_cumulative_rewards.max()},
+                {'tag': 'reward/min', 'simple_value': all_cumulative_rewards.min()},
+                {'tag': 'reward/hitted', 'simple_value': hitted}
+            ], eval_step)
+
+            time_elapse = (time.time() - start_time) / 60
+            rewards_sorted = ", ".join([f"{i:.1f}" for i in sorted(all_cumulative_rewards)])
+            print(f'{eval_step}, {time_elapse:.2f}m, rewards {rewards_sorted}, hitted {hitted}')
+            eval_step += 1
+
+    def _run_learner_server(self):
+        app = Flask('learner')
+
+        import logging
+        log = logging.getLogger('werkzeug')
+        log.setLevel(logging.ERROR)
+
+        @app.route('/get_policy_variables')
+        def get_policy_variables():
+            variables = self.sac.get_policy_variables()
+            return jsonify(variables)
+
+        app.run(host='0.0.0.0', port=self._learner_port)
+
+    def _get_sampled_data(self):
+        while True:
+            try:
+                r = requests.get(f'http://{self._replay_host}:{self._replay_port}/sample')
+            except Exception as e:
+                print('Exception _get_sampled_trans:', e)
+            else:
+                break
+        return r.json()
+
+    def _update_td_errors(self, points, td_errors):
+        while True:
+            try:
+                requests.post(f'http://{self._replay_host}:{self._replay_port}/update',
+                              json={
+                                  'points': points,
+                                  'td_errors': td_errors
+                              })
+            except Exception as e:
+                print('Exception _update_td_errors:', e)
+            else:
+                break
+
+    def _run_training_client(self):
+        # asyncio.run(self._websocket_server.send_to_all('aaa'))
+        t_evaluation = threading.Thread(target=self._start_policy_evaluation)
+
+        while True:
+            data = self._get_sampled_data()
+
+            if data:
+                if not t_evaluation.is_alive():
+                    t_evaluation.start()
+
+                trans = data['trans']
+                is_weights = data['is_weights']
+
+                is_weights = np.array(is_weights)
+                for i, p in enumerate(trans):
+                    trans[i] = np.array(p)
+
+                s, a, r, s_, done = trans
+                curr_step, td_errors = self.sac.train(s, a, r, s_, done, is_weights)
+
+                self._update_td_errors(data['points'], td_errors.tolist())
+            else:
+                print('sampled None. Wating 5 seconds...')
+                time.sleep(5)
+
+    def _run(self):
+        self._websocket_server = WebsocketServer(self._websocket_port)
+
+        t_learner = threading.Thread(target=self._run_learner_server)
+        t_training = threading.Thread(target=self._run_training_client)
+        t_learner.start()
+        t_training.start()
+
+        asyncio.get_event_loop().run_forever()
+
+
+class WebsocketServer:
+    _websocket_clients = set()
+
+    def __init__(self, port=8890):
+        start_server = websockets.serve(self._websocket_open, '0.0.0.0', port)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(start_server)
+
+    async def _websocket_open(self, websocket, path):
+        self._websocket_clients.add(websocket)
+        self.print_websocket_clients()
+
+        try:
+            async for message in websocket:
+                pass
+        except websockets.ConnectionClosed:
+            self._websocket_clients.remove(websocket)
+            self.print_websocket_clients()
+
+    def print_websocket_clients(self):
+        print(f'{len(self._websocket_clients)} active actors')
+        for i, client in enumerate(self._websocket_clients):
+            print(f'[{i+1}]. {client.remote_address[0]} : {client.remote_address[1]}')
+
+    async def send_to_all(self, message):
+        tasks = []
+        try:
+            for client in self._websocket_clients:
+                tasks.append(client.send(message))
+            await asyncio.gather(*tasks)
+        except websockets.ConnectionClosed:
+            pass
---
+++ ds/simple_boat/main.py
@@ -0,0 +1,17 @@
+import getopt
+import sys
+sys.path.append('..')
+
+node = sys.argv[1]
+
+if node == '--replay':
+    from replay import Replay
+    Replay(sys.argv[2:])
+elif node == '--learner':
+    from learner import Learner
+    Learner(sys.argv[2:])
+elif node == '--actor':
+    from actor import Actor
+    Actor(sys.argv[2:])
+else:
+    print('ERROR')
