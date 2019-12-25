d1eb9d94d524e2d40a469b0d32908aa245bd693f
Fisher Chang <blue_fisher@qq.com>
2019-12-25 19:33:47 +0800

    adjust actor logic
    add PostReward API

--- envs/3dball/sac_hard.py
+++ envs/3dball/sac_hard.py
@@ -49,9 +49,8 @@ class ModelPrediction(tf.keras.Model):
 class ModelQ(tf.keras.Model):
     def __init__(self, state_dim, action_dim):
         super(ModelQ, self).__init__()
-        self.layer_s = tf.keras.layers.Dense(64, activation=tf.nn.relu, **initializer_helper)
-        self.layer_a = tf.keras.layers.Dense(64, activation=tf.nn.relu, **initializer_helper)
         self.sequential_model = tf.keras.Sequential([
+            tf.keras.layers.Dense(64, activation=tf.nn.relu, **initializer_helper),
             tf.keras.layers.Dense(64, activation=tf.nn.relu, **initializer_helper),
             tf.keras.layers.Dense(64, activation=tf.nn.relu, **initializer_helper),
             tf.keras.layers.Dense(1, **initializer_helper)
@@ -60,9 +59,7 @@ class ModelQ(tf.keras.Model):
         self(tf.keras.Input(shape=(state_dim,)), tf.keras.Input(shape=(action_dim,)))
 
     def call(self, inputs_s, inputs_a):
-        ls = self.layer_s(inputs_s)
-        la = self.layer_a(inputs_a)
-        l = tf.concat([ls, la], -1)
+        l = tf.concat([inputs_s, inputs_a], -1)
 
         q = self.sequential_model(l)
         return q
@@ -72,16 +69,14 @@ class ModelPolicy(tf.keras.Model):
     def __init__(self, state_dim, action_dim):
         super(ModelPolicy, self).__init__()
         self.common_model = tf.keras.Sequential([
-            tf.keras.layers.Dense(64, activation=tf.nn.relu, **initializer_helper),
-            tf.keras.layers.Dense(64, activation=tf.nn.relu, **initializer_helper)
+            tf.keras.layers.Dense(64, activation=tf.nn.tanh, **initializer_helper),
+            tf.keras.layers.Dense(64, activation=tf.nn.tanh, **initializer_helper)
         ])
         self.mu_model = tf.keras.Sequential([
-            tf.keras.layers.Dense(64, activation=tf.nn.relu, **initializer_helper),
             tf.keras.layers.Dense(action_dim, activation=tf.nn.tanh, **initializer_helper)
         ])
         self.sigma_model = tf.keras.Sequential([
-            tf.keras.layers.Dense(64, activation=tf.nn.relu, **initializer_helper),
-            tf.keras.layers.Dense(action_dim, **initializer_helper)
+            tf.keras.layers.Dense(action_dim, activation=None, **initializer_helper)
         ])
 
         self.tfpd = tfp.layers.DistributionLambda(make_distribution_fn=lambda t: tfp.distributions.Normal(t[0], t[1]))
