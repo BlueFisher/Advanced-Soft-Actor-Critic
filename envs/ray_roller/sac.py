4e0c737200a8152476f04799c929917ba77d37ba
ZhenyuZhang <blue_fisher@icloud.com>
2019-12-27 10:55:17 +0800

    add burn_in padding in actor
    add episode_buffer_config in replay

--- envs/ray_roller/sac.py
+++ envs/ray_roller/sac.py
@@ -2,53 +2,97 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
 
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 initializer_helper = {
-    'kernel_initializer': tf.truncated_normal_initializer(0, .1),
-    'bias_initializer': tf.constant_initializer(.1)
+    'kernel_initializer': tf.keras.initializers.TruncatedNormal(0, .1),
+    'bias_initializer': tf.keras.initializers.Constant(0.1)
 }
 
 
-class SAC_Custom(object):
-    def _build_q_net(self, s_input, a_input, scope, trainable=True, reuse=False):
-        with tf.variable_scope(scope, reuse=reuse):
-            ls = tf.layers.dense(
-                s_input, 256, activation=tf.nn.relu,
-                trainable=trainable, **initializer_helper
-            )
-            la = tf.layers.dense(
-                a_input, 256, activation=tf.nn.relu,
-                trainable=trainable, **initializer_helper
-            )
-            l = tf.concat([ls, la], 1)
-            l = tf.layers.dense(l, 256, activation=tf.nn.relu, trainable=trainable, **initializer_helper)
-            l = tf.layers.dense(l, 256, activation=tf.nn.relu, trainable=trainable, **initializer_helper)
-            l = tf.layers.dense(l, 256, activation=tf.nn.relu, trainable=trainable, **initializer_helper)
-            q = tf.layers.dense(l, 1, **initializer_helper, trainable=trainable)
+class ModelRNN(tf.keras.Model):
+    def __init__(self, state_dim):
+        super(ModelRNN, self).__init__()
+        self.state_dim = state_dim
+        self.rnn_units = 64
+        self.layer_lstm = tf.keras.layers.GRU(self.rnn_units, return_sequences=True, return_state=True)
 
-            variables = tf.get_variable_scope().global_variables()
+        self.get_call_result_tensors()
 
-        return q, variables
+    def call(self, inputs_s, initial_state):
+        outputs, next_state = self.layer_lstm(inputs_s, initial_state=initial_state)
 
-    def _build_policy_net(self, s_inputs, scope, trainable=True, reuse=False):
-        with tf.variable_scope(scope, reuse=reuse):
-            l = tf.layers.dense(s_inputs, 256, tf.nn.relu, **initializer_helper, trainable=trainable)
-            l = tf.layers.dense(l, 256, tf.nn.relu, **initializer_helper, trainable=trainable)
-            l = tf.layers.dense(l, 256, tf.nn.relu, **initializer_helper, trainable=trainable)
-            l = tf.layers.dense(l, 256, tf.nn.relu, **initializer_helper, trainable=trainable)
+        encoded_s = tf.concat([inputs_s, outputs], -1)
+        return encoded_s, next_state
 
-            mu = tf.layers.dense(l, 256, tf.nn.relu, **initializer_helper, trainable=trainable)
-            mu = tf.layers.dense(mu, self.a_dim, tf.nn.tanh, **initializer_helper, trainable=trainable)
+    def get_call_result_tensors(self):
+        return self(tf.keras.Input(shape=(None, self.state_dim,), dtype=tf.float32),
+                    tf.keras.Input(shape=(self.rnn_units,), dtype=tf.float32))
 
-            sigma = tf.layers.dense(l, 256, tf.nn.relu, **initializer_helper, trainable=trainable)
-            sigma = tf.layers.dense(sigma, self.a_dim, tf.nn.sigmoid, **initializer_helper, trainable=trainable)
-            sigma = sigma + .1
 
-            policy = tfp.distributions.Normal(loc=mu, scale=sigma)
-            action = policy.sample()
+class ModelPrediction(tf.keras.Model):
+    def __init__(self, state_dim, encoded_state_dim, action_dim):
+        super(ModelPrediction, self).__init__()
+        self.seq = tf.keras.Sequential([
+            tf.keras.layers.Dense(128, activation=tf.nn.relu, **initializer_helper),
+            tf.keras.layers.Dense(128, activation=tf.nn.relu, **initializer_helper),
+            tf.keras.layers.Dense(128, activation=tf.nn.relu, **initializer_helper),
+            tf.keras.layers.Dense(state_dim, **initializer_helper)
+        ])
 
-            variables = tf.get_variable_scope().global_variables()
+        self(tf.keras.Input(shape=(encoded_state_dim,)), tf.keras.Input(shape=(action_dim,)))
 
-        return policy, action, variables
+    def call(self, inputs_s, inputs_a):
+        return self.seq(tf.concat([inputs_s, inputs_a], -1))
+
+
+class ModelQ(tf.keras.Model):
+    def __init__(self, state_dim, action_dim):
+        super(ModelQ, self).__init__()
+        self.layer_s = tf.keras.layers.Dense(128, activation=tf.nn.relu, **initializer_helper)
+        self.layer_a = tf.keras.layers.Dense(128, activation=tf.nn.relu, **initializer_helper)
+        self.sequential_model = tf.keras.Sequential([
+            tf.keras.layers.Dense(128, activation=tf.nn.relu, **initializer_helper),
+            tf.keras.layers.Dense(128, activation=tf.nn.relu, **initializer_helper),
+            tf.keras.layers.Dense(1, **initializer_helper)
+        ])
+
+        self(tf.keras.Input(shape=(state_dim,)), tf.keras.Input(shape=(action_dim,)))
+
+    def call(self, inputs_s, inputs_a):
+        ls = self.layer_s(inputs_s)
+        la = self.layer_a(inputs_a)
+        l = tf.concat([ls, la], -1)
+
+        q = self.sequential_model(l)
+        return q
+
+
+class ModelPolicy(tf.keras.Model):
+    def __init__(self, state_dim, action_dim):
+        super(ModelPolicy, self).__init__()
+        self.common_model = tf.keras.Sequential([
+            tf.keras.layers.Dense(128, activation=tf.nn.relu, **initializer_helper),
+            tf.keras.layers.Dense(128, activation=tf.nn.relu, **initializer_helper)
+        ])
+        self.mu_model = tf.keras.Sequential([
+            tf.keras.layers.Dense(128, activation=tf.nn.relu, **initializer_helper),
+            tf.keras.layers.Dense(action_dim, activation=tf.nn.tanh, **initializer_helper)
+        ])
+        self.sigma_model = tf.keras.Sequential([
+            tf.keras.layers.Dense(128, activation=tf.nn.relu, **initializer_helper),
+            tf.keras.layers.Dense(action_dim, activation=tf.nn.sigmoid, **initializer_helper)
+        ])
+
+        self.tfpd = tfp.layers.DistributionLambda(make_distribution_fn=lambda t: tfp.distributions.Normal(t[0], t[1]))
+
+        self(tf.keras.Input(shape=(state_dim,)))
+
+    def call(self, inputs_s):
+        l = self.common_model(inputs_s)
+
+        mu = self.mu_model(l)
+
+        sigma = self.sigma_model(l)
+        sigma = sigma + .1
+
+        return self.tfpd([mu, sigma])
