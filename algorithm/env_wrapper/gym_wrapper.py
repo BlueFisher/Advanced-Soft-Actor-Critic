35e4a0ea3e50a052bc86352bf19f20b580d843bd
ZhenyuZhang <blue_fisher@icloud.com>
2020-02-23 15:33:54 +0800

    add multi agent gym

--- algorithm/env_wrapper/gym_wrapper.py
+++ algorithm/env_wrapper/gym_wrapper.py
@@ -8,28 +8,49 @@ class GymWrapper:
                  env_name=None):
         self.train_mode = train_mode
         self.env_name = env_name
-
-        self._env = gym.make(env_name)
+        self._envs = list()
 
     def init(self):
-        return self._env.observation_space.shape[0], self._env.action_space.shape[0]
+        env = gym.make(self.env_name)
+        self._state_dim = env.observation_space.shape[0]
+        self._action_dim = env.action_space.shape[0]
+
+        return self._state_dim, self._action_dim
 
     def reset(self, reset_config=None):
-        obs = self._env.reset()
+        if reset_config['copy'] != len(self._envs):
+            for env in self._envs:
+                env.close()
 
-        return 1, np.expand_dims(obs, 0)
+            for _ in range(reset_config['copy']):
+                self._envs.append(gym.make(self.env_name))
+
+        obs = np.stack([e.reset() for e in self._envs], axis=0)
+
+        return len(self._envs), obs.astype(np.float32)
 
     def step(self, action):
-        obs, reward, done, info = self._env.step(action[0])
+        obs = np.empty([len(self._envs), self._state_dim], dtype=np.float32)
+        reward = np.empty(len(self._envs), dtype=np.float32)
+        done = np.empty(len(self._envs), dtype=np.bool)
+        max_step = np.full(len(self._envs), False)
+
+        for i, env in enumerate(self._envs):
+            tmp_obs, tmp_reward, tmp_done, info = env.step(action[i])
+            obs[i] = tmp_obs
+            reward[i] = tmp_reward
+            done[i] = tmp_done
 
         if not self.train_mode:
-            self._env.render()
+            self._envs[0].render()
 
         if self.env_name == 'Pendulum-v0':
             reward = reward / 10
 
-        # obs, reward, done, max_step
-        return np.expand_dims(obs, 0), [reward], [done], [False]
+        for i in np.where(done)[0]:
+            obs[i] = self._envs[i].reset()
+
+        return obs, reward, done, max_step
 
     def close(self):
         self._env.close()
