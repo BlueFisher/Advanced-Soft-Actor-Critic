4ee7ae928a88460b985e4c91d0cc45175c811528
Fisher Chang <blue_fisher@qq.com>
2020-03-20 10:04:19 +0800

    add discrete action space feature

--- algorithm/env_wrapper/gym_wrapper.py
+++ algorithm/env_wrapper/gym_wrapper.py
@@ -1,3 +1,5 @@
+import logging
+
 import numpy as np
 import gym
 
@@ -12,15 +14,24 @@ class GymWrapper:
         self.n_agents = n_agents
         self._envs = list()
 
+        self._logger = logging.getLogger('UnityWrapper')
+
     def init(self):
         for _ in range(self.n_agents):
             self._envs.append(gym.make(self.env_name))
 
         env = self._envs[0]
-        self._state_dim = env.observation_space.shape[0]
-        self._action_dim = env.action_space.shape[0]
+        self._logger.info(f'Observation shapes: {env.observation_space}')
+        self._logger.info(f'Action size: {env.action_space}')
 
-        return self._state_dim, self._action_dim
+        self._state_dim = env.observation_space.shape[0]
+        self.is_discrete = isinstance(env.action_space, gym.spaces.Discrete)
+        if self.is_discrete:
+            action_dim = env.action_space.n
+        else:
+            action_dim = env.action_space.shape[0]
+
+        return self._state_dim, action_dim, self.is_discrete
 
     def reset(self, reset_config=None):
         obs = np.stack([e.reset() for e in self._envs], axis=0)
@@ -33,6 +44,9 @@ class GymWrapper:
         done = np.empty(len(self._envs), dtype=np.bool)
         max_step = np.full(len(self._envs), False)
 
+        if self.is_discrete:
+            # Convert one-hot to label
+            action = np.argmax(action, axis=1)
         for i, env in enumerate(self._envs):
             tmp_obs, tmp_reward, tmp_done, info = env.step(action[i])
             obs[i] = tmp_obs
