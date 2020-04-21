5228d74b21dcc0a8bc31c5784f6778dceea0febc
ZhenyuZhang <blue_fisher@icloud.com>
2020-04-21 21:20:32 +0800

    format config

---
+++ check_config.py
@@ -0,0 +1,8 @@
+import sys
+import yaml
+
+
+with open(f'envs/{sys.argv[1]}/config.yaml') as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+print(yaml.dump(config, default_flow_style=False))
