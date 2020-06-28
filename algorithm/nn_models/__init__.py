30330221ba5da55730168a65849db51a307798ea
ZhenyuZhang <blue_fisher@icloud.com>
2020-06-28 10:23:24 +0800

    fix convolutional layer bug

--- algorithm/nn_models/__init__.py
+++ algorithm/nn_models/__init__.py
@@ -14,3 +14,15 @@ def dense(n=64, depth=2, pri=None, post=None):
         l.append(post)
 
     return tf.keras.Sequential(l)
+
+
+def through_conv(x, conv):
+    if len(x.shape) > 4:
+        batch = tf.shape(x)[0]
+        x = tf.reshape(x, [-1, *x.shape[2:]])
+        x = conv(x)
+        x = tf.reshape(x, [batch, -1, x.shape[-1]])
+    else:
+        x = conv(x)
+
+    return x
