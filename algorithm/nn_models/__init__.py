fix gradients overlap fix sac_main time fix unittest nn_model

algorithm/nn_models/__init__.py
@@ -1,10 +1,9 @@
-from .representation import *
-from .policy import *
-from .q import *
-from .predictions import *
 from .exploration import *
 from .layers import *
-
+from .policy import *
+from .predictions import *
+from .q import *
+from .representation import *
 
 # class Noisy(tf.keras.layers.Layer):
 #     '''