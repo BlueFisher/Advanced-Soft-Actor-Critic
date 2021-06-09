33e001f954d43a86466d7d5d59c3f0f9ff5c82d9
Fisher Chang <blue_fisher@qq.com>
2021-06-09 13:31:29 +0800

    add saving model manually

---
+++ envs/usv/nn.py
@@ -0,0 +1,23 @@
+import algorithm.nn_models as m
+
+ModelRep = m.ModelSimpleRep
+
+
+class ModelQ(m.ModelQ):
+    def _build_model(self):
+        return super()._build_model(c_dense_n=64, c_dense_depth=2)
+
+
+class ModelPolicy(m.ModelPolicy):
+    def _build_model(self):
+        return super()._build_model(c_dense_n=64, c_dense_depth=2)
+
+
+class ModelForward(m.ModelForward):
+    def _build_model(self):
+        return super()._build_model(dense_n=self.state_size + self.action_size, dense_depth=2)
+
+
+class ModelRND(m.ModelRND):
+    def _build_model(self):
+        return super()._build_model(dense_n=64, dense_depth=2)
