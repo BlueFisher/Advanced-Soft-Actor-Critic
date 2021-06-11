a7b0208db54e5e61d8fcd1932151bd63e88df370
Fisher Chang <blue_fisher@qq.com>
2021-06-11 18:41:52 +0800

    add comments, fix hitted summary log add xavier_uniform init

--- envs/usv/nn.py
+++ envs/usv/nn.py
@@ -5,12 +5,12 @@ ModelRep = m.ModelSimpleRep
 
 class ModelQ(m.ModelQ):
     def _build_model(self):
-        return super()._build_model(c_dense_n=64, c_dense_depth=2)
+        return super()._build_model(c_dense_n=128, c_dense_depth=2)
 
 
 class ModelPolicy(m.ModelPolicy):
     def _build_model(self):
-        return super()._build_model(c_dense_n=64, c_dense_depth=2)
+        return super()._build_model(c_dense_n=128, c_dense_depth=2)
 
 
 class ModelForward(m.ModelForward):
@@ -20,4 +20,4 @@ class ModelForward(m.ModelForward):
 
 class ModelRND(m.ModelRND):
     def _build_model(self):
-        return super()._build_model(dense_n=64, dense_depth=2)
+        return super()._build_model(dense_n=128, dense_depth=2)
