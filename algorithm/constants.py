5ae2926b759e1cf4d6913958f23863127f65e122
ZhenyuZhang <blue_fisher@icloud.com>
2021-08-12 16:06:01 +0800

    fix episode_size_buffer

--- algorithm/constants.py
+++ algorithm/constants.py
@@ -7,7 +7,7 @@ RECONNECTION_TIME = 2  # If the connection is lost, the reconnection time
 PING_INTERVAL = 5  # Interval time between two pings
 RPC_ERR_RETRY = 5  # Max retry times when rpc error encountered
 
-ELAPSED_REPEAT = 1
+ELAPSED_REPEAT = 50
 
 MAX_EPISODE_SIZE = 500
 EPISODE_QUEUE_SIZE = 5
