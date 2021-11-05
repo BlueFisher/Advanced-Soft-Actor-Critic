c03e9e59299db3d45ec34ba41075081c56840888
ZhenyuZhang <blue_fisher@icloud.com>
2021-11-05 18:49:15 +0800

    add conv1d

--- algorithm/nn_models/layers.py
+++ algorithm/nn_models/layers.py
@@ -46,7 +46,23 @@ class LinearLayers(nn.Module):
         return self.dense(x)
 
 
-def conv_output_shape(
+def conv1d_output_size(
+        l: int,
+        kernel_size: int = 1,
+        stride: int = 1,
+        padding: int = 0,
+        dilation: int = 1):
+
+    from math import floor
+
+    l_out = floor(
+        ((l + (2 * padding) - (dilation * (kernel_size - 1)) - 1) / stride) + 1
+    )
+
+    return l_out
+
+
+def conv2d_output_shape(
     h_w: Tuple[int, int],
     kernel_size: Union[int, Tuple[int, int]] = 1,
     stride: int = 1,
@@ -110,9 +126,59 @@ def convtranspose_output_shape(
     return h, w
 
 
+def default_conv1d(l, channels):
+    conv_1_l = conv1d_output_size(l, 8, 4)
+    conv_2_l = conv1d_output_size(conv_1_l, 4, 2)
+
+    return nn.Sequential(
+        nn.Conv1d(channels, 16, 8, 4),
+        nn.LeakyReLU(),
+        nn.Conv1d(16, 32, 4, 2),
+        nn.LeakyReLU(),
+    ), conv_2_l, 32
+
+
+class Conv1dLayers(nn.Module):
+    def __init__(self, in_l: int, in_channels: int,
+                 conv: Union[str, Tuple[nn.Module, int, int]],
+                 out_dense_n: int = 64, out_dense_depth: int = 0, output_size: int = None):
+        super().__init__()
+
+        if isinstance(conv, str):
+            if conv == 'default':
+                self.conv_layers, l, out_c = default_conv1d(in_l, in_channels)
+            else:
+                raise RuntimeError(f'No pre-defined {conv} convolutional layer')
+        elif isinstance(conv, tuple):
+            self.conv_layers, l, out_c = conv
+        else:
+            raise RuntimeError('Argument conv should a Tuple[nn.Module, Tuple[int, int], int]')
+
+        self.conv_output_size = l * out_c
+
+        self.dense = LinearLayers(
+            self.conv_output_size,
+            out_dense_n,
+            out_dense_depth,
+            output_size
+        )
+        self.output_size = self.dense.output_size
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if len(x.shape) >= 3:
+            batch = x.shape[:-2]
+            x = x.reshape(-1, *x.shape[-2:])
+            x = x.permute([0, 2, 1])
+            hidden = self.conv_layers(x)
+            hidden = hidden.reshape(*batch, self.conv_output_size)
+            return self.dense(hidden)
+        else:
+            raise Exception('The dimension of input should be greater than or equal to 3')
+
+
 def small_visual(height, width, channels):
-    conv_1_hw = conv_output_shape((height, width), 3, 1)
-    conv_2_hw = conv_output_shape(conv_1_hw, 3, 1)
+    conv_1_hw = conv2d_output_shape((height, width), 3, 1)
+    conv_2_hw = conv2d_output_shape(conv_1_hw, 3, 1)
 
     return nn.Sequential(
         nn.Conv2d(channels, 35, [3, 3], [1, 1]),
@@ -123,8 +189,8 @@ def small_visual(height, width, channels):
 
 
 def simple_visual(height, width, channels):
-    conv_1_hw = conv_output_shape((height, width), 8, 4)
-    conv_2_hw = conv_output_shape(conv_1_hw, 4, 2)
+    conv_1_hw = conv2d_output_shape((height, width), 8, 4)
+    conv_2_hw = conv2d_output_shape(conv_1_hw, 4, 2)
 
     return nn.Sequential(
         nn.Conv2d(channels, 16, [8, 8], [4, 4]),
@@ -135,9 +201,9 @@ def simple_visual(height, width, channels):
 
 
 def nature_visual(height, width, channels):
-    conv_1_hw = conv_output_shape((height, width), 8, 4)
-    conv_2_hw = conv_output_shape(conv_1_hw, 4, 2)
-    conv_3_hw = conv_output_shape(conv_2_hw, 3, 1)
+    conv_1_hw = conv2d_output_shape((height, width), 8, 4)
+    conv_2_hw = conv2d_output_shape(conv_1_hw, 4, 2)
+    conv_3_hw = conv2d_output_shape(conv_2_hw, 3, 1)
 
     return nn.Sequential(
         nn.Conv2d(channels, 32, [8, 8], [4, 4]),
@@ -151,7 +217,7 @@ def nature_visual(height, width, channels):
 
 class ConvLayers(nn.Module):
     def __init__(self, in_height: int, in_width: int, in_channels: int,
-                 conv: Union[str, Tuple],
+                 conv: Union[str, Tuple[nn.Module, Tuple[int, int], int]],
                  out_dense_n: int = 64, out_dense_depth: int = 0, output_size: int = None):
         super().__init__()
 
@@ -162,6 +228,8 @@ class ConvLayers(nn.Module):
                 self.conv_layers, (h, w), out_c = simple_visual(in_height, in_width, in_channels)
             elif conv == 'nature':
                 self.conv_layers, (h, w), out_c = nature_visual(in_height, in_width, in_channels)
+            else:
+                raise RuntimeError(f'No pre-defined {conv} convolutional layer')
         elif isinstance(conv, tuple):
             self.conv_layers, (h, w), out_c = conv
         else:
--- envs/realcar/nn_raspi_car.py
+++ envs/realcar/nn_raspi_car.py
@@ -11,28 +11,16 @@ EXTRA_SIZE = 6
 class ModelRep(m.ModelBaseRNNRep):
     def _build_model(self, blur, brightness, need_ray, need_speed):
         assert self.obs_shapes[0] == (84, 84, 3)
-        assert self.obs_shapes[1] == (1442,)  # ray (1 + 360 + 360) * 2
-        assert self.obs_shapes[2] == (6,)  # vector
+        assert self.obs_shapes[1] == (2,)  # ray
+        assert self.obs_shapes[2] == (8,)  # vector
 
         self.need_ray = need_ray
         self.need_speed = need_speed
 
-        self.ray_index = []
-        self.ray_size = 720
-        for i in range(360):
-            self.ray_index.append((i * 2 + 1) * 2)
-            self.ray_index.append((i * 2 + 1) * 2 + 1)
-        for i in reversed(range(360)):
-            self.ray_index.append((i * 2 + 2) * 2)
-            self.ray_index.append((i * 2 + 2) * 2 + 1)
-
         self.conv = m.ConvLayers(84, 84, 3, 'simple',
                                  out_dense_n=64, out_dense_depth=2)
 
-        self.ray_conv = m.Conv1dLayers(720, 2, 'default',
-                                       out_dense_n=64, out_dense_depth=2)
-
-        self.dense = m.LinearLayers(self.conv.output_size + self.ray_conv.output_size,
+        self.dense = m.LinearLayers(self.conv.output_size,
                                     dense_n=64, dense_depth=1)
 
         self.rnn = m.GRU(64 + self.c_action_size, 64, 1)
@@ -57,11 +45,11 @@ class ModelRep(m.ModelBaseRNNRep):
 
     def forward(self, obs_list, pre_action, rnn_state=None):
         vis_cam, ray, vec = obs_list
-        ray = ray[..., self.ray_index]
         vec = vec[..., :-EXTRA_SIZE]
 
         if self.blurrer:
             vis_cam = self.blurrer(vis_cam)
+
         vis_cam = self.brightness(vis_cam)
 
         if not self.need_ray:
@@ -69,11 +57,7 @@ class ModelRep(m.ModelBaseRNNRep):
 
         vis = self.conv(vis_cam)
 
-        ray = ray.view(*ray.shape[:-1], self.ray_size, 2)
-        ray = self.ray_conv(ray)
-
-        vis_ray_concat = self.dense(torch.cat([vis, ray], dim=-1))
-        state, hn = self.rnn(torch.cat([vis_ray_concat, pre_action], dim=-1), rnn_state)
+        state, hn = self.rnn(torch.cat([self.dense(vis), pre_action], dim=-1), rnn_state)
 
         state = torch.cat([state, ray, vec], dim=-1)
 
