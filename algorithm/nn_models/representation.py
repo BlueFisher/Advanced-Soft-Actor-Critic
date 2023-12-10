f295d9a546573e85e267bba33217a83e7443d12b
ZhenyuZhang <blue_fisher@icloud.com>
2023-12-10 09:06:24 +0800

    optimize repr

--- algorithm/nn_models/representation.py
+++ algorithm/nn_models/representation.py
@@ -7,26 +7,37 @@ from torch import nn
 from algorithm.nn_models.layers import LinearLayers
 
 
-class ModelBaseSimpleRep(nn.Module):
-    def __init__(self, 
+class ModelBaseRep(nn.Module):
+    def __init__(self,
                  obs_names: List[str],
                  obs_shapes: List[Tuple],
                  is_target: bool, train_mode: bool,
                  model_abs_dir: Optional[Path] = None, **kwargs):
         super().__init__()
+
         self.obs_names = obs_names
         self.obs_shapes = obs_shapes
         self.is_target = is_target
         self.train_mode = train_mode
         self.model_abs_dir = model_abs_dir
 
-        self._build_model(**kwargs)
+        self._offline_action_index = -1
+        try:
+            self._offline_action_index = self.obs_names.index('_OFFLINE_ACTION')
+        except:
+            pass
 
     def _build_model(self, **kwargs):
         pass
 
     def forward(self, obs_list: List[torch.Tensor]):
-        raise Exception("ModelSimpleRep not implemented")
+        raise Exception("ModelRep not implemented")
+
+    def get_offline_action(self, obs_list: List[torch.Tensor]) -> torch.Tensor:
+        if self._offline_action_index != -1:
+            return obs_list[self._offline_action_index]
+        else:
+            return None
 
     def get_augmented_encoders(self,
                                obs_list: List[torch.Tensor]) -> Union[torch.Tensor, Tuple[torch.Tensor]]:
@@ -38,12 +49,23 @@ class ModelBaseSimpleRep(nn.Module):
         raise Exception("get_state_from_encoders not implemented")
 
 
+class ModelBaseSimpleRep(ModelBaseRep):
+    def __init__(self,
+                 obs_names: List[str],
+                 obs_shapes: List[Tuple],
+                 is_target: bool, train_mode: bool,
+                 model_abs_dir: Optional[Path] = None, **kwargs):
+        super().__init__(obs_names, obs_shapes, is_target, train_mode, model_abs_dir)
+
+        self._build_model(**kwargs)
+
+
 class ModelSimpleRep(ModelBaseSimpleRep):
     def forward(self, obs_list):
         return torch.cat([o for o, os in zip(obs_list, self.obs_shapes) if len(os) == 1], dim=-1)
 
 
-class ModelBaseRNNRep(nn.Module):
+class ModelBaseRNNRep(ModelBaseRep):
     def __init__(self,
                  obs_names: List[str],
                  obs_shapes: List[Tuple],
@@ -52,20 +74,12 @@ class ModelBaseRNNRep(nn.Module):
                  model_abs_dir: Optional[Path] = None,
                  use_dilation=False,  # For option critic
                  **kwargs):
-        super().__init__()
-        self.obs_names = obs_names
-        self.obs_shapes = obs_shapes
+        super().__init__(obs_names, obs_shapes, is_target, train_mode, model_abs_dir)
         self.d_action_sizes = d_action_sizes
         self.c_action_size = c_action_size
-        self.train_mode = train_mode
-        self.is_target = is_target
-        self.model_abs_dir = model_abs_dir
         self.use_dilation = use_dilation
 
         self._build_model(**kwargs)
-
-    def _build_model(self, **kwargs):
-        pass
 
     def forward(self,
                 obs_list: List[torch.Tensor],
@@ -81,9 +95,6 @@ class ModelBaseRNNRep(nn.Module):
         """
         raise Exception("ModelRNNRep not implemented")
 
-    def get_augmented_encoders(self, obs_list: List[torch.Tensor]) -> Union[torch.Tensor, Tuple[torch.Tensor]]:
-        raise Exception("get_augmented_encoders not implemented")
-
     def get_state_from_encoders(self,
                                 obs_list: List[torch.Tensor],
                                 encoders: Union[torch.Tensor, Tuple[torch.Tensor]],
@@ -93,7 +104,7 @@ class ModelBaseRNNRep(nn.Module):
         raise Exception("get_state_from_encoders not implemented")
 
 
-class ModelBaseAttentionRep(nn.Module):
+class ModelBaseAttentionRep(ModelBaseRep):
     def __init__(self,
                  obs_names: List[str],
                  obs_shapes: List[Tuple],
@@ -102,14 +113,9 @@ class ModelBaseAttentionRep(nn.Module):
                  model_abs_dir: Optional[Path] = None,
                  use_dilation=False,  # For option critic
                  **kwargs):
-        super().__init__()
-        self.obs_names = obs_names
-        self.obs_shapes = obs_shapes
+        super().__init__(obs_names, obs_shapes, is_target, train_mode, model_abs_dir)
         self.d_action_sizes = d_action_sizes
         self.c_action_size = c_action_size
-        self.train_mode = train_mode
-        self.is_target = is_target
-        self.model_abs_dir = model_abs_dir
         self.use_dilation = use_dilation
 
         self._build_model(**kwargs)
@@ -124,9 +130,6 @@ class ModelBaseAttentionRep(nn.Module):
                 query_only_attend_to_rest_key=False,
                 padding_mask: Optional[torch.Tensor] = None):
         raise Exception('ModelAttentionRep not implemented')
-
-    def get_augmented_encoders(self, obs_list: List[torch.Tensor]) -> Union[torch.Tensor, Tuple[torch.Tensor]]:
-        raise Exception("get_augmented_encoders not implemented")
 
     def get_state_from_encoders(self,
                                 index: torch.Tensor,
