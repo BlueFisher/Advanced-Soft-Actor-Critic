83ff8393928ce33fb1aa7f80391aa3992a9d0fcb
ZhenyuZhang <blue_fisher@qq.com>
2025-06-15 11:13:32 +0800

    refactor VisionTransformer class, add initialization and input processing methods

--- algorithm/nn_models/layers/image_layers.py
+++ algorithm/nn_models/layers/image_layers.py
@@ -1,8 +1,10 @@
+import math
+from functools import partial
 from typing import Callable
 
 import torch
 from torch import nn
-from torchvision.models.vision_transformer import VisionTransformer
+from torchvision.models.vision_transformer import Encoder
 
 from .linear_layers import LinearLayers
 
@@ -244,7 +246,79 @@ class ConvTransposeLayers(nn.Module):
             raise Exception('The dimension of input should be greater than or equal to 2')
 
 
-class VisionTransformer(VisionTransformer):
+class VisionTransformer(nn.Module):
+    def __init__(
+        self,
+        image_size: int,
+        in_channels: int,
+        patch_size: int,
+        num_layers: int,
+        num_heads: int,
+        hidden_dim: int,
+        mlp_dim: int,
+        dropout: float = 0.0,
+        attention_dropout: float = 0.0,
+        norm_layer: Callable[..., torch.nn.Module] = partial(nn.LayerNorm, eps=1e-6),
+    ):
+        super().__init__()
+        torch._assert(image_size % patch_size == 0, "Input shape indivisible by patch size!")
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.hidden_dim = hidden_dim
+        self.mlp_dim = mlp_dim
+        self.attention_dropout = attention_dropout
+        self.dropout = dropout
+        self.norm_layer = norm_layer
+
+        self.conv_proj = nn.Conv2d(
+            in_channels=in_channels, out_channels=hidden_dim, kernel_size=patch_size, stride=patch_size
+        )
+
+        seq_length = (image_size // patch_size) ** 2
+
+        # Add a class token
+        self.class_token = nn.Parameter(torch.zeros(1, 1, hidden_dim))
+        seq_length += 1
+
+        self.encoder = Encoder(
+            seq_length,
+            num_layers,
+            num_heads,
+            hidden_dim,
+            mlp_dim,
+            dropout,
+            attention_dropout,
+            norm_layer,
+        )
+        self.seq_length = seq_length
+
+        # Init the patchify stem
+        fan_in = self.conv_proj.in_channels * self.conv_proj.kernel_size[0] * self.conv_proj.kernel_size[1]
+        nn.init.trunc_normal_(self.conv_proj.weight, std=math.sqrt(1 / fan_in))
+        if self.conv_proj.bias is not None:
+            nn.init.zeros_(self.conv_proj.bias)
+
+    def _process_input(self, x: torch.Tensor) -> torch.Tensor:
+        n, c, h, w = x.shape
+        p = self.patch_size
+        torch._assert(h == self.image_size, f"Wrong image height! Expected {self.image_size} but got {h}!")
+        torch._assert(w == self.image_size, f"Wrong image width! Expected {self.image_size} but got {w}!")
+        n_h = h // p
+        n_w = w // p
+
+        # (n, c, h, w) -> (n, hidden_dim, n_h, n_w)
+        x = self.conv_proj(x)
+        # (n, hidden_dim, n_h, n_w) -> (n, hidden_dim, (n_h * n_w))
+        x = x.reshape(n, self.hidden_dim, n_h * n_w)
+
+        # (n, hidden_dim, (n_h * n_w)) -> (n, (n_h * n_w), hidden_dim)
+        # The self attention layer expects inputs in the format (N, S, E)
+        # where S is the source sequence length, N is the batch size, E is the
+        # embedding dimension
+        x = x.permute(0, 2, 1)
+
+        return x
+
     def forward(self, x: torch.Tensor):
         if x.dim() >= 4:
             batch = x.shape[:-3]
